64c8f33b32c914a290b30451ed384b7c695099d7
Liubko89 <142455025+Liubko89@users.noreply.github.com>
2023-11-05 22:04:06 +0000

    1st try

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,17 @@
-'use strict';
+function filterArray(numbers, value) {
+  let newArray = [];
 
-function getElementWidth(content, padding, border) {
-  return (
-    Number.parseFloat(content) +
-    Number.parseFloat(padding) * 2 +
-    Number.parseFloat(border) * 2
-  );
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > value) {
+      newArray.push(numbers[i]);
+    }
+  }
+
+  return newArray;
 }
 
-console.log(getElementWidth('50px', '8px', '4px'));
-console.log(getElementWidth('60px', '12px', '8.5px'));
-console.log(getElementWidth('200px', '0px', '0px'));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
+console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
+console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
+console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
+console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
